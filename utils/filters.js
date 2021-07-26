34d9f1fc063ffffbcd511fb6de5b0d9c1254f216
Eric Wallace <eric@ecrosstexas.com>
2021-07-26 00:24:28 -0500

    day 004
    webmentions

--- utils/filters.js
+++ utils/filters.js
@@ -40,4 +40,20 @@ module.exports = {
         return arr.slice(0, count)
       },
 
+/**
+ * Webmetnions, thx Sia.codes
+ */
+  getWebmentionsForUrl: (webmentions, url) => {
+    return webmentions.children.filter(entry => entry['wm-target'] === url)
+  },
+  size: (mentions) => {
+    return !mentions ? 0 : mentions.length
+  },
+  webmentionsByType: (mentions, mentionType) => {
+    return mentions.filter(entry => !!entry[mentionType])
+  },
+  readableDateFromISO: (dateStr, formatStr = "dd LLL yyyy 'at' hh:mma") => {
+    return DateTime.fromISO(dateStr).toFormat(formatStr);
+  }
+
 }
