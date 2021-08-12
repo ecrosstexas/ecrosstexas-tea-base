54d86989c2d38b940704fbc5b0ff5ac44a25b5bb
Eric Wallace <eric@ecrosstexas.com>
2021-08-11 23:57:51 -0500

    Netlify
    Changes from eleventy-netlify-boilerplate

--- .eleventy.js
+++ .eleventy.js
@@ -114,10 +114,10 @@ eleventyConfig.addFilter("readableDate", dateObj => {
 	eleventyConfig.addPassthroughCopy('src/*.png')
 	eleventyConfig.addPassthroughCopy('src/*.jpg')
 	eleventyConfig.addPassthroughCopy('src/*.ico')
-	eleventyConfig.addPassthroughCopy('src/robots.txt')
 	eleventyConfig.addPassthroughCopy('src/assets/images/')
 	eleventyConfig.addPassthroughCopy('src/assets/svg/')
 	eleventyConfig.addPassthroughCopy('src/assets/css/prism*.css')
+  eleventyConfig.addPassthroughCopy("src/content/admin");
 
   /**
 	 * Add layout aliases
